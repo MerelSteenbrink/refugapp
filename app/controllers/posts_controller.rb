f31fd60cdd530e17859b7ea25e94b1b2fa44e336
Gina Goldberg <gina.goldberg@hotmail.com>
2016-06-14 17:02:29 +0200

    add controller and basic views for posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author_id = current_user.id
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:subject, :content)
+  end
+end

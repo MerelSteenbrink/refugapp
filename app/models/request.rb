ac04ea3d75db3e626d78dae8709591e4914c6410
Aafke Smal <aafke.smal@gmail.com>
2016-06-14 14:17:21 +0200

    Add modelrelations

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,3 +1,5 @@
 class Request < ActiveRecord::Base
+
   belongs_to :post
+  belongs_to :messenger, class_name: 'User', foreign_key: :messenger_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :posts, foreign_key: :author_id
+  has_many :received_requests, class_name: 'Request', through: :posts
+  has_many :sent_requests, class_name: 'Request', foreign_key: :messenger_id
+
 end

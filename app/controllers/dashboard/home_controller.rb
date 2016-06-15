a32cee8f381aa25948a2a2895242b9fdcd2d6ae1
Aafke Smal <aafke.smal@gmail.com>
2016-06-15 11:17:36 +0200

    add personal posts on dashboard

--- app/controllers/dashboard/home_controller.rb
+++ app/controllers/dashboard/home_controller.rb
@@ -1,7 +1,19 @@
 class Dashboard::HomeController < ApplicationController
+  before_action :find_user, only: [:index]
+   before_action :find_posts, only: [:index]
 
   def index
-    @user = current_user
+
+  end
+
+  private
+
+  def find_user
+    @user = current_user if current_user
+  end
+
+  def find_posts
+    @posts = Post.where(author: current_user )
   end
 
 end

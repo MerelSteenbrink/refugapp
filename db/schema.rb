52e3270aff7fe5377cc2d18480069af01ca1816f
Geert Keularts <geertkeularts@gmail.com>
2016-06-15 12:14:37 +0200

    add username, postal, latitude and longitude to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160614143936) do
+ActiveRecord::Schema.define(version: 20160615100049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,10 @@ ActiveRecord::Schema.define(version: 20160614143936) do
     t.string   "last_name"
     t.string   "token"
     t.datetime "token_expiry"
+    t.string   "username"
+    t.string   "postal_code"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree

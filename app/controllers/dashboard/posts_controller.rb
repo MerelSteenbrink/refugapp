04dc7f86ab692595ec0a2869c58b548d505ba505
Aafke Smal <aafke.smal@gmail.com>
2016-06-15 15:10:18 +0200

    show all sent/received requests + style

--- app/controllers/dashboard/posts_controller.rb
+++ app/controllers/dashboard/posts_controller.rb
@@ -1,4 +1,7 @@
 class Dashboard::PostsController < ApplicationController
+  before_action :find_user, only: [:index]
+  before_action :find_posts, only: [:index]
+  before_action :find_received_requests, only: [:index]
 
   def index
 
@@ -7,5 +10,21 @@ class Dashboard::PostsController < ApplicationController
   def destroy
 
   end
+
+  private
+
+  def find_user
+    @user = current_user if current_user
+  end
+
+   def find_posts
+    @posts = Post.where(author: current_user )
+  end
+
+   def find_received_requests
+    @received_requests = current_user.received_requests
+  end
+
+
 
 end

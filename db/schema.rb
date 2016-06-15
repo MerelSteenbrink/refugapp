f2938b3a0fd69579af2f9e19daf3cb8a421b677f
Gina Goldberg <gina.goldberg@hotmail.com>
2016-06-15 11:18:57 +0200

    Add necessary geo columns to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160614143936) do
+ActiveRecord::Schema.define(version: 20160615091516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,11 @@ ActiveRecord::Schema.define(version: 20160614143936) do
     t.string   "subject"
     t.text     "content"
     t.integer  "author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.float    "latitude"
+    t.float    "longitude"
+    t.string   "postal_code"
   end
 
   create_table "requests", force: :cascade do |t|

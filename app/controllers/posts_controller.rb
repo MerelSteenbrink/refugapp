5a8074674cac7eae21ace264d55d7ffd6daf094b
Gina Goldberg <gina.goldberg@hotmail.com>
2016-06-16 12:40:27 +0200

    add a working map based on city and postcode

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,6 +34,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:subject, :content)
+    params.require(:post).permit(:subject, :content, :city, :postal_code)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,14 @@ class Post < ActiveRecord::Base
   has_many :received_requests, class_name: 'Request'
   belongs_to :author,  class_name: 'User', foreign_key: :author_id
 
-  geocoded_by :postal_code
+  geocoded_by :address
   after_validation :geocode, if: :postal_code_changed?
+
+ private
+
+  def address
+    "#{postal_code} #{city}"
+  end
+
 
 end

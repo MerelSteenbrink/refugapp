09da5c1ef6dad87138dd8aab75039a0b37539f69
Aafke Smal <aafke.smal@gmail.com>
2016-06-16 15:59:39 +0200

    Validate city and postal code

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,9 @@ class Post < ActiveRecord::Base
   has_many :received_requests, class_name: 'Request', dependent: :destroy
   belongs_to :author,  class_name: 'User', foreign_key: :author_id
 
-  validates :subject, :content, :author_id, presence: true
-  validates :subject, uniqueness: {scope: :author_id, message: "You've already made a post with a subject"}
+  validates :subject, :content, :author_id, :postal_code, :city, presence: true
+  validates :subject, uniqueness: {scope: :author_id, message: "You've already made a post with this subject"}
+  validates_format_of :postal_code, with: /\A\d{4}\s?[a-zA-Z]{2}\z/i, :message => "Please fill in a right postal code"
 
   geocoded_by :address
   after_validation :geocode, if: :postal_code_changed?

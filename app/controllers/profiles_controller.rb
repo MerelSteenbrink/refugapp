d481f19bb7e962c555699b4ce448948fed71a3aa
Geert Keularts <geertkeularts@gmail.com>
2016-06-16 12:31:46 +0200

    add edit profile page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,15 @@
 class ProfilesController < ApplicationController
+  before_action :find_user, only: [:show, :edit]
+
   def show
+  end
+
+  def edit
+  end
+
+  private
+
+  def find_user
     @user = current_user
   end
 end

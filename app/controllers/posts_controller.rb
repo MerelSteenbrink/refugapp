f103ab708987d452694fd6d64c039b7ca489d010
MerelSteenbrink <merel.steenbrink@gmail.com>
2016-06-17 14:39:55 +0200

    dutchie can only see refugee psots and alrevez

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,15 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @posts = Post.all
+    @postsall= Post.all
+    if !current_user
+      @posts = @postsall
+    elsif current_user.kind == "dutchie"
+      @posts = @postsall.select { |post| User.find(post.author_id).kind == 'refugee' }
+    else
+      @posts = @postsall.select { |post| User.find(post.author_id).kind == 'dutchie' }
+    end
+
     @posties = @posts.sort_by{ |post| post[:updated_at] }.reverse
 
 

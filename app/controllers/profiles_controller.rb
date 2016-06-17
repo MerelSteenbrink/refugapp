c9c39cbaa108f890be26a480fcc7f9e1fdad6755
Aafke Smal <aafke.smal@gmail.com>
2016-06-17 15:20:07 +0200

    Add city to edit profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -22,6 +22,6 @@ class ProfilesController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:id, :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :created_at, :updated_at, :provider, :uid, :picture, :first_name, :last_name, :token, :token_expiry, :username, :postal_code, :kind, :photo, :photo_cache, :description)
+    params.require(:user).permit(:id, :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :created_at, :updated_at, :provider, :uid, :picture, :first_name, :last_name, :token, :token_expiry, :username, :postal_code, :kind, :photo, :photo_cache, :description, :city)
   end
 end

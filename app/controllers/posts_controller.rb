697bf9701b429f6beaa139cd771663a43705bd34
Gina Goldberg <gina.goldberg@hotmail.com>
2016-06-21 14:48:37 +0200

    implement a double search filter on posts index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,49 +2,69 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @postsall= Post.all
-    if !current_user
-      @posts = @postsall
-    elsif current_user.kind == "dutchie"
-      @posts = @postsall.select { |post| User.find(post.author_id).kind == 'refugee' }
-    else
-      @posts = @postsall.select { |post| User.find(post.author_id).kind == 'dutchie' }
+
+    @kind = params[:kind]
+    @category = params[:category]
+
+    #If a user has not searched/clicked on a specific kind of post, we decide what to show him based on the user.kind (refugee/dutchie) and show the posts of the opposite kind.
+
+    if current_user && @kind == nil
+      if current_user.kind == "refugee"
+        @kind = "dutchie"
+      else
+        @kind = "refugee"
+      end
     end
 
-    @posties = @posts.sort_by{ |post| post[:updated_at] }.reverse
+    #Based on the kind that was clicked by the user, or the default we provide based on the above lines of code, we filter!
 
+    if @kind == "dutchie" || @kind == "refugee"
+      @posts = Post.joins(:author).where(users: {kind: @kind})
+    else
+      @posts = Post.all
+    end
+
+
+    if @category && @category != "all"
+      @posts = @posts.where(category: @category)
+    end
+
+
+
+    # Order posts based on descending order (newest on top)
+    @posties = @posts.order(updated_at: :desc)
 
     @markers = Gmaps4rails.build_markers(@posts) do |post, marker|
       marker.lat post.latitude
       marker.lng post.longitude
 
-      # For clickable markers that show a little text box
-      marker.infowindow render_to_string(:partial => "/posts/map_box", locals: {post: post})
-    end
+    # For clickable markers that show a little text box
+    marker.infowindow render_to_string(:partial => "/posts/map_box", locals: {post: post})
   end
+end
 
-  def show
-    @post = Post.find(params[:id])
-    @author = User.find(@post.author_id)
+def show
+  @post = Post.find(params[:id])
+  @author = User.find(@post.author_id)
+end
+
+def new
+  @post = Post.new
+end
+
+def create
+  @post = Post.new(post_params)
+  @post.author_id = current_user.id
+  if @post.save
+    redirect_to post_path(@post)
+  else
+    render :new
   end
+end
 
-  def new
-    @post = Post.new
-  end
+private
 
-  def create
-    @post = Post.new(post_params)
-    @post.author_id = current_user.id
-    if @post.save
-      redirect_to post_path(@post)
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def post_params
-    params.require(:post).permit(:subject, :content, :city, :postal_code, :category)
-  end
+def post_params
+  params.require(:post).permit(:subject, :content, :city, :postal_code, :category)
+end
 end

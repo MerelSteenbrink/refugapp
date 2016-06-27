9c1de971bbd435df5af7340a9e438074eff605fc
MerelSteenbrink <merel.steenbrink@gmail.com>
2016-06-27 11:55:50 +0200

    add second button

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,22 +2,22 @@ module PostsHelper
 
 
   def filter_kind_css_class(filter_kind)
-    css_class = "btn btn-default btn-block btn-lg"
+    css_class = "btn-border-black"
 
     # if the button is active/ selected add the class active to the button
     if filter_kind == @kind
-      css_class << " active"
+      css_class << " active-black"
     end
     return css_class
   end
 
 
   def filter_category_css_class(filter_category)
-    css_class = "btn btn-default btn-lg"
+    css_class = "btn-border-blue-select"
 
     # if the button is active/ selected add the class active to the button
     if filter_category == @category
-      css_class << " active"
+      css_class << " active-blue"
     end
     return css_class
 

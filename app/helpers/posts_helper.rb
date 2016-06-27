a27a166d3cf6431c1a0e0cf76e74d7c58de353ea
MerelSteenbrink <merel.steenbrink@gmail.com>
2016-06-27 14:21:13 +0200

    buttons next to eachother

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -13,11 +13,11 @@ module PostsHelper
 
 
   def filter_category_css_class(filter_category)
-    css_class = "btn-border-blue-select"
+    css_class = "btn-border-orange-select"
 
     # if the button is active/ selected add the class active to the button
     if filter_category == @category
-      css_class << " active-blue"
+      css_class << " active-orange"
     end
     return css_class
 

26122583050431ee01acdbb9fbcd28970584af07
MerelSteenbrink <merel.steenbrink@gmail.com>
2016-06-30 17:27:34 +0200

    not scrolling toooo much anymore

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,14 +1,14 @@
-/*$('.chat').scrollTop(
+/*/*$('.chat').scrollTop(
   $('.chat')[0].scrollHeight
   );*/
 
 
-/*$(document).ready(function(){
-    $('container').scrollTop($('ul li').last().position().top);
-});*/
-
 $(document).ready(function(){
-
 var myDiv = $(".chat");
 myDiv.animate({ scrollTop: myDiv.prop("scrollHeight") - myDiv.height() }, 500);
+
 });
+
+/*$(document).ready(function(){
+
+});*/
